71fbbd8128a598e9d83180e4c689f0dd2ec82850
JP <dragonginger10@gmail.com>
2023-11-04 13:28:02 -0500

    set up main function to access from pyproject

---
+++ pyobd/__init__.py
@@ -0,0 +1,6 @@
+from pyobd.pyobd import MyApp
+
+def main():
+    app = MyApp(0)
+    app.MainLoop()
+
--- pyobd/__main__.py
+++ pyobd/__main__.py
@@ -1,5 +1,4 @@
-from pyobd.pyobd import MyApp
+from pyobd import main
 
 if __name__ == "__main__":
-    app = MyApp(0)
-    app.MainLoop()
+    main()
